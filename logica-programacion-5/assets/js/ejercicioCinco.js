b212f19384341fd52a6375647a536d5a2cbaf92c
Luis M. Ortiz-Galvez <luismauricio.ortizgz@outlook.com>
2022-12-19 23:59:00 -0600

    Comenzando ejercicio 5

---
+++ logica-programacion-5/assets/js/ejercicioCinco.js
@@ -0,0 +1,20 @@
+//COMIENZA Solicitar número secreto
+let nombre =  prompt('¿Cuál es tu nombre?');
+let saludo = ('Hola ' + nombre + '. Te quiero retar a adivinar el número secreto. Coloca un número positivo a continuación.');
+alert(saludo);
+
+let num =  Number(prompt('¡Comencemos! Coloca un número que sea mayor que cero (0):'));
+//TERMINA Solicitar número secreto
+
+/* Creación de número aleatorio entre 1 y 100:
+function numAleatorio() {
+    var aleatorio = Math.round(Math.random()*100);
+} */
+
+//COMIENZA Obtención del número aleatorio
+//El número escogido es:
+//TERMINA Obtención del número aleatorio
+
+//COMIENZA 
+
+//TERMINA
